84ca4dd1fd906d7f43703c5c516ba6760c0216f3
pabarov <pressian.abarov@gmail.com>
2014-03-15 11:40:59 +0200

    Create inits.js

---
+++ js/inits.js
@@ -0,0 +1,22 @@
+
+var initCamera = function() {
+
+	camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 10000);
+	scene.add(camera);
+	camera.position.z = -50;
+};
+
+var initRenderer = function() {
+
+	renderer = new THREE.WebGLRenderer({antialias:true});
+	renderer.setClearColor(0x303030);
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.sortObjects = false;
+	container = document.getElementById('ThreeJS');
+	container.appendChild(renderer.domElement);
+};
+
+var initLight = function() {
+
+	scene.add(new THREE.AmbientLight(0x808080));
+};
