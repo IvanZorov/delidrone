71d7db33dcae211dbaa27fb2018ac082cfa1a40e
pabarov <pressian.abarov@gmail.com>
2014-04-13 14:35:06 +0300

    Create cameras.js

---
+++ js/cameras.js
@@ -0,0 +1,10 @@
+var initCamera = function() {
+
+	camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 30000);
+	camera.update = function() {
+		controls.update(clock.getDelta());
+	};
+	camera.position.set(0, 800, 0);
+	camera.lookAt(world.position);
+	scene.add(camera);
+};
