8341c0e2d03bd383a0f871125b41982338a84d20
pabarov <pressian.abarov@gmail.com>
2014-04-13 14:34:04 +0300

    Update addToScene.js

--- js/addToScene.js
+++ js/addToScene.js
@@ -1,16 +1,4 @@
 
-var addDroneToScene = function (geometry, materials) {
-
-	model = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial(materials));
-	model.scale.set(1,1,1);
-	drone = model;
-	scene.add(drone);
-
-	drone.light = new THREE.DirectionalLight(0x88cc00, 2, 100);
-	drone.light.position.y = drone.position.y+5;
-	scene.add(drone.light);
-};
-
 var addWorldToScene = function () {
 
 	var floor = new THREE.Mesh(new THREE.PlaneGeometry(100000, 100000, 1, 1), new	THREE.MeshPhongMaterial({ color:0x050505, side: THREE.BackSide }));
@@ -21,20 +9,20 @@ var addWorldToScene = function () {
 	var buildingMesh= new THREE.Mesh( geometry );
 
 	var cityGeometry= new THREE.Geometry();
-	for( var i = 0; i < 2000; i ++ ) {
+	for( var i = 0; i < 200; i ++ ) {
 
-		buildingMesh.position.x = Math.floor( Math.random() * 200 - 100 ) * 40;
-		buildingMesh.position.z = Math.floor( Math.random() * 200 - 100 ) * 40;
+		buildingMesh.position.x = Math.floor( Math.random() * 100 - 100 ) * 70;
+		buildingMesh.position.z = Math.floor( Math.random() * 100 - 100 ) * 70;
 		buildingMesh.rotation.y = Math.random()*Math.PI*2;
-		buildingMesh.scale.x = Math.random() * 100 + 10;
-		buildingMesh.scale.y = (Math.random() * buildingMesh.scale.x) * 30 + 10;
-		buildingMesh.scale.z = Math.random() * 100 + 10;
+		buildingMesh.scale.x = Math.random() * 50 + 50;
+		buildingMesh.scale.y = Math.random() * buildingMesh.scale.x * 25 + 50;
+		buildingMesh.scale.z = Math.random() * 50 + 50;
 		THREE.GeometryUtils.merge( cityGeometry, buildingMesh );
 	}
 
-	var material  = new THREE.MeshLambertMaterial({color:0x202020});
+	var material  = new THREE.MeshBasicMaterial({color:0x0020ff});
 	var cityMesh = new THREE.Mesh(cityGeometry, material );
-
+	world = cityMesh;
 	scene.add(cityMesh);
 	scene.add(floor);
 };
